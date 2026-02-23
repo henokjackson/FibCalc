44d7f016b3c52d62b45c63d0db1e6e7d6ce0a60b
Henok Jackson <henokjackson@gmail.com>
2026-02-23 23:57:24 +0530

    Updated program run path in server

--- Server.py
+++ Server.py
@@ -5,5 +5,5 @@ server = FastAPI()
 
 @server.get("/calculate/{range}")
 async def CalculateFibonacci(range: int):
-	output = subprocess.run(["/app/Fibonacci", "--range="+str(range)], capture_output=True, text=True)
+	output = subprocess.run(["./Fibonacci", "--range="+str(range)], capture_output=True, text=True)
 	return output.stdout.split("\n\n")[1];
